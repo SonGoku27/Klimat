e482cb6d15978d841b2e3c213556bcf5e4a3ab42
SonGoku27 <152280209+SonGoku27@users.noreply.github.com>
2025-04-14 20:11:40 +0500

    Update main.py
    - Проверка данные на выбросы и адекватность данных. И обработка на исключение выбросов

--- Klimat_now/main.py
+++ Klimat_now/main.py
@@ -1,6 +1,19 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
-df = pd.read_csv('../PythonProject4/Data.csv', sep=';', encoding='Utf-8')
+
+def emissions(zxc):
+    # выбросы - утепление(max - 2.08), температура_воздуха_в_помещении(max - 77.00), скорость_воздуха(max - 63.8300),
+    # рост(min - 5.23), среднемесячная_температура_на_улице(max - 328.00)
+    for i in ['утепление', 'температура_воздуха_в_помещении', 'скорость_воздуха', 'рост',
+              'среднемесячная_температура_на_улице']:
+        zxc = zxc[(zxc[i] > zxc[i].quantile(0.01)) & (zxc[i] < zxc[i].quantile(0.99))]
+    return zxc
+
+
+df = pd.read_csv('Data.csv', sep=';', encoding='Utf-8')
+pd.set_option('display.max_columns', None)
 df.dropna()
 df.columns = df.columns.str.replace(' ', '_').str.lower()
 df['год'] = df['год'].astype('int')
@@ -11,3 +24,8 @@ df_no_duplicates = df.drop_duplicates(
             'окно', 'двери', 'отопление'],
     keep=False)
 
+df['возраст'].plot(kind='hist', density=1, bins=20, stacked=False, alpha=.5, color='grey')
+
+
+df1 = emissions(df)
+print(df1.describe())
