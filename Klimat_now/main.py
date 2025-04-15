589386d4ed5d87373c5604097767f2dbcfdeb301
SonGoku27 <152280209+SonGoku27@users.noreply.github.com>
2025-04-15 19:05:32 +0500

    Update main.py
    МЫ должны любить наших мам

--- Klimat_now/main.py
+++ Klimat_now/main.py
@@ -6,6 +6,11 @@ import numpy as np
 def emissions(zxc):
     # выбросы - утепление(max - 2.08), температура_воздуха_в_помещении(max - 77.00), скорость_воздуха(max - 63.8300),
     # рост(min - 5.23), среднемесячная_температура_на_улице(max - 328.00)
+    #zxc = zxc[zxc['утепление'] < zxc['утепление'].quantile(0.99)]
+    #zxc = zxc[zxc['температура_воздуха_в_помещении'] < zxc['температура_воздуха_в_помещении'].quantile(0.99)]
+    #zxc = zxc[zxc['скорость_воздуха'] < zxc['скорость_воздуха'].quantile(0.99)]
+    #zxc = zxc[zxc['среднемесячная_температура_на_улице'] < zxc['среднемесячная_температура_на_улице'].quantile(0.99)]
+    #zxc = zxc[(zxc['рост'] > zxc['рост'].quantile(0.01))]
     for i in ['утепление', 'температура_воздуха_в_помещении', 'скорость_воздуха', 'рост',
               'среднемесячная_температура_на_улице']:
         zxc = zxc[(zxc[i] > zxc[i].quantile(0.01)) & (zxc[i] < zxc[i].quantile(0.99))]
@@ -23,9 +28,13 @@ df_no_duplicates = df.drop_duplicates(
             'предпочтительное_изменение_движения_воздуха', 'занавески', 'вентилятор',
             'окно', 'двери', 'отопление'],
     keep=False)
-
+print()
+df['рост'].fillna(df['рост'].describe().median(), inplace=True)
+df['вес'].fillna(df['вес'].describe().median(), inplace=True)
 df['возраст'].plot(kind='hist', density=1, bins=20, stacked=False, alpha=.5, color='grey')
 
 
 df1 = emissions(df)
 print(df1.describe())
+
+print(df1)
